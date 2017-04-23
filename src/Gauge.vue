75cf183382ee21bb49cbcc673d666fea988e1785
phphe <phphe@outlook.com>
2017-04-23 16:58:16 +0800

    resolved

--- src/Gauge.vue
+++ src/Gauge.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-<!--     <radial-gauge options="{ value: 233 }"></radial-gauge>
-    <linear-gauge :value="377"></linear-gauge> -->
+  <h1>This is the name</h1>
+    <radial-gauge options="{ value: 233 }"></radial-gauge>
+    <linear-gauge :value="377"></linear-gauge>
     <h1>This is the name</h1>
   </div>
 </template>
 
 <script>
- //import LinearGauge  from 'vue-canvas-gauges'
+ import LinearGauge from 'vue-canvas-gauges/src/LinearGauge.vue'
 
 export default {
   components: {
- //   LinearGauge
+   LinearGauge
   }
 }
 </script>
